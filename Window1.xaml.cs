252dc100f4db4c326d574b94ff415c7bbc367bd3
anettefonnes <anette_fonnes@hotmail.com>
2015-03-04 15:49:42 +0100

    GODKJENT

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -34,6 +34,7 @@ namespace WpfApplication1 {
 
 			dc = new DataContext("Data Source=" + host + ";Initial Catalog=" + database + ";User ID=" + user + ";Password=" + pass);
 
+
 			students = dc.GetTable<Student>();
 			grades = dc.GetTable<Grade>();
             course = dc.GetTable<Course>();
@@ -48,8 +49,6 @@ namespace WpfApplication1 {
                        select grade.grade;
 
             gradeBox.DataContext = piss;
-
-
 		}
 
         private void courseList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -66,15 +65,38 @@ namespace WpfApplication1 {
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            char grad = (char)gradeBox.SelectedItem;
+            char grad = ((string)((ComboBoxItem)gradeBox.SelectedItem).Content).ToCharArray().First();
+            if (grad == 'F')
+            {
+               var filter = dc.GetTable<Student>().
+               Join(grades, st => st.id, gr => gr.studentid, (st, gr) => new { st, gr }).
+               Join(course, grr => grr.gr.coursecode, cr => cr.coursecode, (grr, cr) => new { grr, cr }).
+               Where(m => m.grr.gr.grade.Equals(grad)).
+               Select(m => new { m.grr.st.studentname, m.cr.coursecode, m.grr.gr.grade });
 
-            var filter = dc.GetTable<Student>().
-                Join(grades, st => st.id, gr => gr.studentid, (st,gr) => new {st, gr}).
-                Join(course, grr => grr.gr.coursecode, cr => cr.coursecode, (grr,cr) => new{ grr, cr }).
-                Where(m => m.grr.gr.grade.Equals(grad)).
-                Select(m => new { m.grr.st.studentname, m.cr.coursename, m.grr.gr.grade } );
+                /* Comments/suggestions from Remy Monsen
+               var filter2 = dc.GetTable<Student>()
+                   .Join(grades, s => s.id, g => g.studentid, (s, g) => new { s.studentname, g.coursecode, g.grade })
+                   .Where(m => m.grade.Equals(grad));
 
-            gradeList.DataContext = filter;
+
+               DataClasses1DataContext dx = new DataClasses1DataContext();
+
+               var filter3 = dx.grades.Where(x => x.grade1.Equals(grad)).Select(x => new { x.student.id, x.coursecode, x.grade1 });
+                */
+               gradeList.DataContext = filter;
+            }
+            else
+            {
+               var filter = dc.GetTable<Student>().
+               Join(grades, st => st.id, gr => gr.studentid, (st, gr) => new { st, gr }).
+               Join(course, grr => grr.gr.coursecode, cr => cr.coursecode, (grr, cr) => new { grr, cr }).
+               Where(m => (m.grr.gr.grade.CompareTo(grad)) <= 0).
+               Select(m => new { m.grr.st.studentname, m.cr.coursecode, m.grr.gr.grade });
+
+               gradeList.DataContext = filter;
+            }
+
         }
 
         private void search_Click(object sender, RoutedEventArgs e)
@@ -83,11 +105,11 @@ namespace WpfApplication1 {
             studentList.DataContext = filter;
         }
 
-        private void studentList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void search_Click2(object sender, KeyEventArgs e)
         {
-
+            var filter = dc.GetTable<Student>().Where(st => st.studentname.Contains(nameSearch.Text));
+            studentList.DataContext = filter;
         }
-
 
 	}
 }
